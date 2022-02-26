20c376b81125b4155ccd9a7b4bee474a1bda2a36
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-02-26 11:25:34 -0600

    useEffect w/ CustomHook

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CounterApp } from './components/01-useState/CounterApp';
 import { CounterWithHook } from './components/01-useState/CounterWithHook';
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 import { SimpleForm } from './components/02-useEffect/SimpleForm';
 import { HookApp } from './HookApp';
 
 ReactDOM.render(
-    <SimpleForm />,
+    <FormWithCustomHook />,
   document.getElementById('root')
 );
 
